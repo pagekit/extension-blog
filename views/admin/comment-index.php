c7bf9577c2160a1fffc2c55de2970235fd3139b4
janschoenherr <jan@yootheme.com>
2015-11-13 19:24:34 +0100

    update

--- views/admin/comment-index.php
+++ views/admin/comment-index.php
@@ -35,7 +35,7 @@
         <table class="uk-table uk-table-hover pk-table-large">
             <thead>
                 <tr>
-                    <th class="pk-table-width-minimum"><input type="checkbox" v-check-all="selected: input[name=id]" number></th>
+                    <th class="pk-table-width-minimum"><input type="checkbox" v-check-all:selected.literal="input[name=id]" number></th>
                     <th class="pk-table-min-width-300" colspan="2">{{ 'Comment' | trans }}</th>
                     <th class="pk-table-width-100 uk-text-center">
                         <input-filter :title="'Status' | trans" :value.sync="config.filter.status" :options="statusOptions"></input-filter>
