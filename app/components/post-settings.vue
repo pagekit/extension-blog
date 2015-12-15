daf6ecbc01b7fd1d733802f9c784ac48ec944fb0
Malte <malte@yootheme.com>
2015-12-15 16:15:43 +0100

    remove v-checkbox

--- app/components/post-settings.vue
+++ app/components/post-settings.vue
@@ -62,7 +62,7 @@
                     <span class="uk-form-label">{{ 'Restrict Access' | trans }}</span>
                     <div class="uk-form-controls uk-form-controls-text">
                         <p v-for="role in data.roles" class="uk-form-controls-condensed">
-                            <label><input type="checkbox" :value="role.id" v-checkbox="post.roles" number> {{ role.name }}</label>
+                            <label><input type="checkbox" :value="role.id" v-model="post.roles" number> {{ role.name }}</label>
                         </p>
                     </div>
                 </div>
