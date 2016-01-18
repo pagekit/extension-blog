208ce50c73f4f4abe084b501ecb032839382dba5
Malte-Christian Scharenberg <scharenberg91@googlemail.com>
2016-01-18 16:58:38 +0100

    cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,13 +14,6 @@ module.exports = [
         output: {
             filename: "./app/bundle/[name].js"
         },
-        externals: {
-            "lodash": "_",
-            "jquery": "jQuery",
-            "uikit": "UIkit",
-            "vue": "Vue",
-            "site": "Site"
-        },
         module: {
             loaders: [
                 { test: /\.vue$/, loader: "vue" }
