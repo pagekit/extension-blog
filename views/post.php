912d765a641469481689ecc2683cde00d0a53fa0
Malte <malte@yootheme.com>
2016-03-30 16:46:42 +0200

    Revert "post - add schema.org microdata"
    This reverts commit 5a8ca11929fc6edfcdebcc9f9705e2a5092b258d.

--- views/post.php
+++ views/post.php
@@ -1,16 +1,18 @@
 <?php $view->script('post', 'blog:app/bundle/post.js', 'vue') ?>
 
-<article class="uk-article" itemscope itemtype="http://schema.org/Article">
+<article class="uk-article">
 
     <?php if ($image = $post->get('image.src')): ?>
     <img src="<?= $image ?>" alt="<?= $post->get('image.alt') ?>">
     <?php endif ?>
 
-    <h1 class="uk-article-title" itemprop="headline"><?= $post->title ?></h1>
+    <h1 class="uk-article-title"><?= $post->title ?></h1>
 
-    <p class="uk-article-meta" itemprop="author"><?= __('Written by %name% on %date%', ['%name%' => '<span itemprop="author">'.$post->user->name.'</span>', '%date%' => '<time datetime="'.$post->date->format(\DateTime::ATOM).'" itemprop="dateCreated" v-cloak>{{ "'.$post->date->format(\DateTime::ATOM).'" | date "longDate" }}</time>' ]) ?></p>
+    <p class="uk-article-meta">
+        <?= __('Written by %name% on %date%', ['%name%' => $post->user->name, '%date%' => '<time datetime="'.$post->date->format(\DateTime::ATOM).'" v-cloak>{{ "'.$post->date->format(\DateTime::ATOM).'" | date "longDate" }}</time>' ]) ?>
+    </p>
 
-    <div class="uk-margin" itemprop="articleBody"><?= $post->content ?></div>
+    <div class="uk-margin"><?= $post->content ?></div>
 
     <?= $view->render('blog/comments.php') ?>
 
